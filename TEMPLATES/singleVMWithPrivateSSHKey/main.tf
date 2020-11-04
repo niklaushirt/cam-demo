e6eabd2e929bdf2f39a933cbd1b9931193295fd2
Niklaus Hirt <niklaushirt@gmail.com>
2020-11-04 14:29:42 +0100

    CPU Hot Add

--- TEMPLATES/singleVMWithPrivateSSHKey/main.tf
+++ TEMPLATES/singleVMWithPrivateSSHKey/main.tf
@@ -65,7 +65,7 @@ resource "vsphere_virtual_machine" "vm" {
   wait_for_guest_net_routable = false
   wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout  = 0
-  cpu_hot_add_enabled = 1
+  cpu_hot_add_enabled = true
   ignored_guest_ips = [ "192.168.10.155" ]
   tags = vsphere_tag.ibm_terraform_automation_tags[*].id
   clone {
