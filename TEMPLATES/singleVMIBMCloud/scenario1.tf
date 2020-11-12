aecd61d7c94c92c3abbadd205f6f6be1114a4dbb
Niklaus Hirt <niklaushirt@gmail.com>
2020-11-12 10:27:57 +0100

    IBMVM

--- TEMPLATES/singleVMIBMCloud/scenario1.tf
+++ TEMPLATES/singleVMIBMCloud/scenario1.tf
@@ -62,5 +62,13 @@ output "vm_public" {
 }
 
 output "vm_private" {
+  value = "${tls_private_key.mykey.private_key_pem}"
+}
+
+output "vm_public_urlenc" {
+  value = "${urlencode(tls_private_key.mykey.public_key_openssh)}"
+}
+
+output "vm_private_urlenc" {
   value = "${urlencode(tls_private_key.mykey.private_key_pem)}"
 }
